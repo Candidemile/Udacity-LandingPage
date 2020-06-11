f967943078a80206ccb935c951883a057461aa02
Candid_emile <doctor_net@mail.ru>
2020-06-11 08:55:02 +0300

    Add navbar generation, some CSS styles

--- js/app.js
+++ js/app.js
@@ -34,7 +34,30 @@
 */
 
 // build the nav
+const buildNavBar = function() {
+    const fragment = document.createDocumentFragment();
+    const sections = document.querySelectorAll("section");
+    const navBar = document.getElementById("navbar__list");
 
+    for (section of sections) {
+        const newElement = document.createElement('li');
+        newElement.classList.add("menu__link");
+        // newElement.innerText = section.id;
+
+        const link = document.createElement('a');
+        link.innerText = section.dataset.nav;
+        link.href = `#${section.id}`;
+        newElement.appendChild(link);
+        
+
+        fragment.appendChild(newElement)
+        console.log(fragment);
+    }
+
+    navBar.appendChild(fragment);
+}
+
+buildNavBar();
 
 // Add class 'active' to section when near top of viewport
 
